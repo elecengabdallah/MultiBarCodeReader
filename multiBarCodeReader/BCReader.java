8291d760809b6deda8009634aecb977b6389fade
AbdAllah Farag <elecengabd_allah@yahoo.com>
2016-09-25 17:53:56 +0200

    still developing in BCReader class

--- multiBarCodeReader/BCReader.java
+++ multiBarCodeReader/BCReader.java
@@ -13,15 +13,18 @@ import org.apache.commons.net.telnet.TerminalTypeOptionHandler;
 
 public class BCReader implements TelnetNotificationHandler {
 	
+	private TelnetClient telnetSocket;
 	private InputStream in;
 	private OutputStream out;
-	private TelnetClient telnetSocket;
 	
 	private String serverAddress = null;
 	private int serverPort = 23;
+	private String userName = "admin\r\n";
+	private String password = "\r\n";
 	
 	private String MAC = null;
 	
+	private final static String GET_MAC_COMMAND = "||>get device.mac-address\r\n";
 	
 	/***
 	 * Default constructor, sets the serverAddress and use default Telnet port 23;
@@ -41,10 +44,48 @@ public class BCReader implements TelnetNotificationHandler {
 		this.serverPort = serverPort;
 	}
 	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/***
+	 * send messages to Barcode Reader
+	 * @param msg input ... string to be sent.
+	 */
+	public void sendMsg(String msg) {
+		
+		byte[] outstr = new byte[1024];
+		int outstrLength = 0;
+		
+		outstr = msg.getBytes();
+		outstrLength = outstr.length;
+		
+		try {
+			out.write(outstr, 0, outstrLength);
+			out.flush();
+		} catch (Exception e) {
+			System.out.println("Error sending msg to server!!!!"); ///***must be modified***
+		}
+	}
+	
 	public boolean start() {
 		
+		//create telnetSocket instance
 		telnetSocket = new TelnetClient();
 		
+		//set option handlers
 		TerminalTypeOptionHandler ttopt = new TerminalTypeOptionHandler("VT100", false, false, true, false);
 		EchoOptionHandler echoopt = new EchoOptionHandler(true, false, true, false);
 		SuppressGAOptionHandler gaopt = new SuppressGAOptionHandler(true, true, true, true);
@@ -53,14 +94,37 @@ public class BCReader implements TelnetNotificationHandler {
 			telnetSocket.addOptionHandler(ttopt);
 			telnetSocket.addOptionHandler(echoopt);
 			telnetSocket.addOptionHandler(gaopt);
-		} catch (InvalidTelnetOptionException e) {
-			System.out.println("Error registering option handlers1: " + e.getMessage()); ///****must be modified****
-		} catch (IOException e) {
-			System.out.println("Error registering option handlers2: " + e.getMessage());///****must be modified****
-		} 
+		} catch (Exception e) {
+			System.out.println("Error registering option handlers!!!"); ///****must be modified****
+			return false;
+		}
+		
+		//connect to barcode reader
+		try {
+			telnetSocket.connect(serverAddress, serverPort);
+		} catch (Exception e) {
+			System.out.println("Error connecting to Barcode Reader at IP: " + serverAddress + " !!!"); ///***must be modified***
+			return false;
+		}
+		
+		//get input and output streams
+		try {
+			in = telnetSocket.getInputStream();
+			out = telnetSocket.getOutputStream();
+		} catch (Exception e) {
+			System.out.println("Error getting input and output streams for Barcode reader at IP: " + serverAddress + " !!!"); ///***must be modified***
+			return false;
+		}
+		
+		//send user name and password
+		sendMsg(userName);
+		sendMsg(password);
+		
 		
 		return true;
 	}
+	
+	
 	
 	@Override
 	public void receivedNegotiation(int negotioation_code, int option_code) {
